03950ec880be5f7822ef09e46e071bf0d9857696
hibahdesu <hibahsindi@gmail.com>
2025-07-14 12:48:08 +0300

    Add the tasks

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-let tasks = [];
+let tasks = [
+    {
+        "title": "Complete the task",
+        "date": "2023-10-01",
+        "description": "Finish the assigned task by the end of the day.",
+        "finished": false
+    },
+    {
+        "title": "Complete the task",
+        "date": "2023-10-01",
+        "description": "Finish the assigned task by the end of the day.",
+        "finished": false
+    },
+    {
+        "title": "Complete the task",
+        "date": "2023-10-01",
+        "description": "Finish the assigned task by the end of the day.",
+        "finished": false
+    },
+];
